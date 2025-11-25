0b1839d595c5ed7c4668a9e5f3c46e1d3c28c572
Darren004 <166741643+Stones004@users.noreply.github.com>
2025-11-25 19:39:07 +0530

    Update urls.py

--- yoyo_project/yoyo_app/urls.py
+++ yoyo_project/yoyo_app/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('home/', views.home, name='home'),
     path('api/hotels/', views.hotels_near_location, name="hotels_near_location"),
+    path('api/register/', views.register_user, name='register_user'),
+    path('api/login/', views.login_user, name='login_user'),
+    path('logout/', views.logout_user, name='logout_user'),
     path('hotel/<int:address_id>/', views.hotel_detail, name='hotel_detail'),
 ]
